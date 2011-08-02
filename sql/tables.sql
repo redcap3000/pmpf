0afe7d4e2e676077f5b3280c6847f6be7eb22ac9
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-08-02 17:09:39 -0400

    updated variable names

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,11 +1,21 @@
 CREATE TABLE mp_groups(
   usergroup SERIAL PRIMARY KEY,
-  group_title varchar(255) NOT NULL,
-  group_alias varchar(255) NOT NULL,
-  group_level INTEGER NOT NULL
+  g_title varchar(255) NOT NULL,
+  g_alias varchar(255) NOT NULL,
+  g_level INTEGER NOT NULL
 );
+
+CREATE TABLE  pmpf_vars(
+	var_id SERIAL PRIMARY KEY,
+	usergroup integer REFERENCES mp_groups default 3,
+	var_name varchar(512) NOT NULL,
+	var_type var_types default 'parsed_var',
+	var_container varchar(512)[2] NOT NULL DEFAULT array['[',']'],
+	var_value varchar(512)
+);
+
 -- so we can have a default value... cant seem to select a null integer column to filter them in/out
-INSERT INTO mp_groups (group_name,usergroup,group_level) VALUES 
+INSERT INTO mp_groups (g_title,usergroup,g_level) VALUES 
 ('System Administrators','SystemGOD',1),
 ('Module Administrators','ModulesGOD',2),
 ('Guests','ViewPublished',100),
@@ -13,8 +23,7 @@ INSERT INTO mp_groups (group_name,usergroup,group_level) VALUES
 ('Normal User','normal',4),
 ('employees','employees',4);
 
-
-CREATE table mp_versions(
+CREATE table pmpf_versions(
 	v_id serial PRIMARY KEY,
 	r_id integer,
 	v_table varchar(512) NOT NULL,
@@ -44,18 +53,18 @@ CREATE TABLE mp_content(
 CREATE TABLE mp_blocks(
   id SERIAL PRIMARY KEY,
   usergroup integer REFERENCES mp_groups default 3,
-  block_name varchar(255) NOT NULL,
+  b_name varchar(255) NOT NULL,
   urls VARCHAR(512) NOT NULL,
-  block_type block_types,
-  block_order SMALLINT,
+  b_type block_types,
+  b_order SMALLINT,
   status statuses DEFAULT 'active'
   ) inherits (mp_content);
   -- ideally should make something to generate this, or a function so
   -- users can easily add new logs 
 
 CREATE TABLE mp_users(
-  userid SERIAL PRIMARY KEY,
-  username varchar(100) NOT NULL,
+  useid SERIAL PRIMARY KEY,
+  username varchar(512) NOT NULL,
   full_name varchar(255)NOT NULL,
   pass_word varchar(100),
   usergroup int NOT NULL,
@@ -67,7 +76,7 @@ CREATE TABLE mp_users(
   randkey varchar(255),
   is_active smallint,
   UNIQUE (username,email)
-);
+)inherits pg_users;
 
 CREATE TABLE mp_sessions(
   session_id SERIAL PRIMARY KEY,
